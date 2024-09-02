75c79e8ac4931c2c54c4ee69b99c1c3868465bb4
Anwaar Shah <anwaarshah194@gmail.com>
2024-09-02 12:29:57 +0500

    whyUs and services sections add

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import HeroSection from './heroSection/HeroSection'
 import Product from './ProductSection/Product'
+import WhyUs from './WhyUsSection/WhyUs'
+import Services from './ServicesSection/Services'
 
 export default function Home() {
   return (
     <div>
       <HeroSection/>
       <Product/>
+      <WhyUs/>
+      <Services/>
     </div>
   )
 }
